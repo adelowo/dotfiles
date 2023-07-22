e3ad1ac38b6d177b6d12ae7e98ac92282e07b196
Lanre Adelowo <yo@lanre.wtf>
2023-07-22 12:57:33 +0100

    update testing config

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -24,6 +24,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.java" },
     { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.test.core" },
     -- import/override with your plugins
     { import = "plugins" },
   },
