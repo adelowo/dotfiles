9ea6006ae7041b7fc60deaa5afd85899f5bede9b
Lanre Adelowo <yo@lanre.wtf>
2023-08-17 02:47:51 +0100

    run goimports when saving a file

--- nvim/lua/plugins/go.lua
+++ nvim/lua/plugins/go.lua
@@ -7,6 +7,16 @@ return {
   },
   config = function()
     require("go").setup()
+
+    -- Run gofmt + goimport on save
+    local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = "*.go",
+      callback = function()
+        require("go.format").goimport()
+      end,
+      group = format_sync_grp,
+    })
   end,
   event = { "CmdlineEnter" },
   ft = { "go", "gomod" },
