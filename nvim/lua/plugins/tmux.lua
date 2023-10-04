4134f783c8b6a14e9442850d4717ea0c38369e14
Lanre Adelowo <yo@lanre.wtf>
2023-10-04 06:23:28 +0100

    update nvim tmux integration

--- nvim/lua/plugins/tmux.lua
+++ nvim/lua/plugins/tmux.lua
@@ -1,16 +1,12 @@
 return {
-  "alexghergh/nvim-tmux-navigation",
-  config = function()
-    require("nvim-tmux-navigation").setup({
-      disable_when_zoomed = true, -- defaults to false
-      keybindings = {
-        left = "<C-h>",
-        down = "<C-j>",
-        up = "<C-k>",
-        right = "<C-l>",
-        last_active = "<C-\\>",
-        next = "<C-Space>",
-      },
-    })
-  end,
+  {
+    "numToStr/Navigator.nvim",
+    config = true,
+    keys = {
+      { "<C-h>", "<cmd>NavigatorLeft<cr>", desc = "Move left" },
+      { "<C-l", "<cmd>NavigatorRight<cr>", desc = "Move right" },
+      { "<C-j>", "<cmd>NavigatorDown<cr>", desc = "Move down" },
+      { "<C-k>", "<cmd>NavigatorUp<cr>", desc = "Move up" },
+    },
+  },
 }
