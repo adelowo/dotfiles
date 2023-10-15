8bbe4cee44571dcfbdcd6e12911528258c98cf19
Lanre Adelowo <yo@lanre.wtf>
2023-10-15 02:16:54 +0100

    add catpuppin theme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -4,7 +4,16 @@ return {
 
   { "dracula/vim" },
   { "shaunsingh/nord.nvim" },
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha",
+      })
+    end,
+  },
 
   {
     "LazyVim/LazyVim",
