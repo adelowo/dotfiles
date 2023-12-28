12b719a25ea9cd4eae08e3161ba82f5b600ac41c
Lanre Adelowo <yo@lanre.wtf>
2023-12-28 19:50:30 +0100

    disable mouse

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -21,7 +21,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "monokai-pro",
+      colorscheme = "catppuccin",
     },
   },
 }
--- nvim/lua/plugins/hardtime.lua
+++ nvim/lua/plugins/hardtime.lua
@@ -1,3 +1,10 @@
+vim.keymap.set("", "<up>", "<nop>", { noremap = true })
+vim.keymap.set("", "<down>", "<nop>", { noremap = true })
+vim.keymap.set("i", "<up>", "<nop>", { noremap = true })
+vim.keymap.set("i", "<down>", "<nop>", { noremap = true })
+
+vim.opt.mouse = ""
+
 return {
   {
     "m4xshen/hardtime.nvim",
