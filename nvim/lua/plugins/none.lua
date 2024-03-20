5b3671bc62cf1786c8308e9318748ae291a67144
Lanre Adelowo <yo@lanre.wtf>
2024-03-20 18:07:16 +0100

    none ls plugin

---
+++ nvim/lua/plugins/none.lua
@@ -0,0 +1,22 @@
+return {
+  "nvimtools/none-ls.nvim",
+  optional = true,
+  dependencies = {
+    {
+      "williamboman/mason.nvim",
+      opts = function(_, opts)
+        opts.ensure_installed = opts.ensure_installed or {}
+        vim.list_extend(opts.ensure_installed, { "gomodifytags", "impl" })
+      end,
+    },
+  },
+  opts = function(_, opts)
+    local nls = require("null-ls")
+    opts.sources = vim.list_extend(opts.sources or {}, {
+      nls.builtins.code_actions.gomodifytags,
+      nls.builtins.code_actions.impl,
+      -- nls.builtins.formatting.goimports,
+      -- nls.builtins.formatting.gofumpt,
+    })
+  end,
+}
