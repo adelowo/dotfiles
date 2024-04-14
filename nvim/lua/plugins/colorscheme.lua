9ebac4d85a9535efa1a2335d87893d426b325cd7
Lanre Adelowo <yo@lanre.wtf>
2024-04-14 12:32:12 +0100

    use kanagwa colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -19,9 +19,53 @@ return {
     end,
   },
   {
+    "rebelot/kanagawa.nvim",
+    config = function()
+      local my_colors = {
+        waveBlue1 = "#363646",
+        sumiInk1 = "#16161D",
+        roninYellow = "#F5DD8E",
+      }
+
+      require("kanagawa").setup({
+        dimInactive = true,
+        colors = {
+          palette = my_colors,
+          theme = {
+            all = {
+              ui = {
+                bg_gutter = "none",
+              },
+            },
+          },
+        },
+        functionStyle = { bold = true },
+        typeStyle = { bold = true },
+        overrides = function(colors)
+          local theme = colors.theme
+          return {
+            TelescopeTitle = { fg = theme.ui.special, bold = true },
+            TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+            TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+
+            Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
+            PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+            PmenuSbar = { bg = theme.ui.bg_m1 },
+            PmenuThumb = { bg = theme.ui.bg_p2 },
+          }
+        end,
+      })
+
+      -- vim.api.nvim_set_hl
+      vim.api.nvim_set_hl(0, "@markdownUrl", { underline = true, italic = true })
+      -- Load the colorscheme
+      vim.cmd([[colorscheme kanagawa-wave]])
+    end,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "kanagawa",
     },
   },
 }
