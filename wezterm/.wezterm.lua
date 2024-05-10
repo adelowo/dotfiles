6d681dca27da20037b0c6ba0ec1fb1de128292a0
Lanre Adelowo <yo@lanre.wtf>
2024-05-10 20:06:25 +0100

    switch to monaspace radon

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -9,7 +9,9 @@ local config = wezterm.config_builder()
 config.color_scheme = "Batman"
 
 -- and finally, return the configuration to wezterm
-config.font = wezterm.font("Fira Code")
+config.font = wezterm.font("Monaspace Radon")
+config.font_size = 13
+
 -- You can specify some parameters to influence the font selection;
 -- for example, this selects a Bold, Italic font variant.
 -- config.font = wezterm.font("JetBrains Mono", { weight = "Bold", italic = true })
