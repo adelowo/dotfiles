45d2c7f31753e841c042a6da2f4f8208e568a772
Lanre Adelowo <yo@lanre.wtf>
2024-05-13 02:58:20 +0100

    update

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,18 +1,10 @@
 local wezterm = require("wezterm")
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
 config.color_scheme = "Batman"
 
--- and finally, return the configuration to wezterm
 config.font = wezterm.font("Monaspace Radon")
 config.font_size = 13
 
--- You can specify some parameters to influence the font selection;
--- for example, this selects a Bold, Italic font variant.
--- config.font = wezterm.font("JetBrains Mono", { weight = "Bold", italic = true })
 return config
