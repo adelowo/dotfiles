5d9887e455529f8573a8a6927a72a49eb7d2069d
Lanre Adelowo <yo@lanre.wtf>
2024-05-17 13:19:48 +0100

    update font size

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -5,6 +5,6 @@ local config = wezterm.config_builder()
 config.color_scheme = "Batman"
 
 config.font = wezterm.font("Monaspace Radon")
-config.font_size = 13
+config.font_size = 16
 
 return config
