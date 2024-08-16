026c9d82c1b6b03b261a101bc05520590978d1b4
Lanre Adelowo <yo@lanre.wtf>
2024-08-16 23:21:44 +0100

    add rayso plugin

---
+++ nvim/lua/plugins/rayso.lua
@@ -0,0 +1,13 @@
+return {
+  "TobinPalmer/rayso.nvim",
+  cmd = { "Rayso" },
+  config = function()
+    require("rayso").setup({
+      options = {
+        background = true, -- If the screenshot should have a background.
+        dark_mode = true, -- If the screenshot should be in dark mode.
+        theme = "tailwind", -- Theme
+      },
+    })
+  end,
+}
