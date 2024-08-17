bd1fcd9247fbe3795c2be138167dd1e343e9aaf8
Lanre Adelowo <yo@lanre.wtf>
2024-08-17 06:26:11 +0100

    hide tabs

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -3,6 +3,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.color_scheme = "Kanagawa (Gogh)"
+config.hide_tab_bar_if_only_one_tab = true
 
 config.font = wezterm.font("Jetbrains Mono")
 config.font_size = 20
