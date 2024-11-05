241ecbba079032b2c269fa59876e18281cacce86
Lanre Adelowo <yo@lanre.wtf>
2024-11-05 12:46:53 +0100

    add zenbones

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -78,6 +78,12 @@ return {
     "nyoom-engineering/oxocarbon.nvim",
   },
   {
+    "zenbones-theme/zenbones.nvim",
+    dependencies = "rktjmp/lush.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       -- colorscheme = "kanagawa-wave",
