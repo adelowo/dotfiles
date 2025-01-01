1cf145e6b6b56cf72672aa741f5e6c600d20cf2c
Lanre Adelowo <yo@lanre.wtf>
2025-01-01 20:58:38 +0100

    use Berkely mono

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,11 +2,15 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
--- config.color_scheme = "Kanagawa (Gogh)"
 config.color_scheme = "Kanagawa Dragon (Gogh)"
 config.hide_tab_bar_if_only_one_tab = true
 
-config.font = wezterm.font("Monaspace Radon")
-config.font_size = 20
+config.font = wezterm.font({
+	-- Berkely mono. Font was stored as TX-02 after download/purchase
+	-- too lazy to rename
+	family = "TX-02",
+	-- weight = "Bold",
+})
+config.font_size = 22
 
 return config
