cacfe56a3b2913c3b14ee0c483f75c4e452ca027
Lanre Adelowo <yo@lanre.wtf>
2025-01-04 21:59:32 +0100

    berkely mono

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -6,9 +6,7 @@ config.color_scheme = "Kanagawa Dragon (Gogh)"
 config.hide_tab_bar_if_only_one_tab = true
 
 config.font = wezterm.font({
-	-- Berkely mono. Font was stored as TX-02 after download/purchase
-	-- too lazy to rename
-	family = "TX-02",
+	family = "Berkeley Mono",
 	-- weight = "Bold",
 })
 config.font_size = 22
