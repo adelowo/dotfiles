a78e32cda6d290b73aee22b8b2250430cd8edfb7
Lanre Adelowo <yo@lanre.wtf>
2025-11-25 09:44:00 +0100

    fix neotest

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -1,28 +1,36 @@
 return {
-  { "nvim-neotest/neotest-plenary" },
-  {
-    "nvim-neotest/neotest-go",
-  },
-  {
-    "rouge8/neotest-rust",
-  },
-  {
-    "lawrence-laz/neotest-zig",
-  },
-  -- {
-  --   "alfaix/neotest-gtest",
-  -- },
   {
     "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      {
+        "fredrikaverpil/neotest-golang",
+        -- version = "*",
+        build = function()
+          vim.system({ "go", "install", "gotest.tools/gotestsum@latest" }):wait()
+        end,
+      },
+    },
     opts = {
       adapters = {
-        "neotest-plenary",
-        "neotest-go",
-        "neotest-python",
-        "neotest-rust",
-        "neotest-zig",
+        -- "neotest-plenary",
+        -- "neotest-go",
+        -- "neotest-python",
+        -- "neotest-rust",
+        -- "neotest-zig",
         -- "neotest-gtest",
       },
     },
+    config = function()
+      local config = {
+        runner = "gotestsum",
+      }
+      require("neotest").setup({
+        adapters = {
+          require("neotest-golang")(config),
+        },
+      })
+    end,
   },
 }
